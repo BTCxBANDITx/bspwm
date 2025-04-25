1b377c4937b490d887909e817c49f486da6c09d0
HACK3R_B4NDIT <141981595+BTCxBANDITx@users.noreply.github.com>
2025-04-24 20:16:42 -0700

    Add files via upload

---
+++ themes/dream_theme/theme.bash
@@ -0,0 +1,75 @@
+# Colors
+background='#16040f'
+foreground='#c4f0f0'
+
+color0='#16040f'
+color1='#9EAE4E'
+color2='#3D7887'
+color3='#31999D'
+color4='#579C9F'
+color5='#36CCD0'
+color6='#61DDE0'
+color7='#c4f0f0'
+
+color8='#89a8a8'
+color9='#9EAE4E'
+color10='#3D7887'
+color11='#31999D'
+color12='#579C9F'
+color13='#36CCD0'
+color14='#61DDE0'
+color15='#c4f0f0'
+
+accent='#abb2bf'
+color_red='#ff0000'
+color_green='#00ff00'
+color_yellow='#ffff00'
+
+
+# Wallpaper
+wallpaper='/home/b4nd1t/Pictures/archcraft.jpg'
+
+# Polybar
+polybar_font='Irish Grover:size=14;3'
+
+# Rofi
+rofi_font='Reggae One 14'
+rofi_icon='Hack'
+
+# Geany
+geany_colors='cyber-sugar.conf'
+geany_font='Typewriter 18'
+
+# Appearance
+gtk_font='Reggae One 14'
+gtk_theme='Hack'
+icon_theme='Hack'
+cursor_theme='LyraB'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x36'
+dunst_origin='top-right'
+dunst_font='Germania One 12'
+dunst_border='1'
+dunst_separator='1'
+
+# Picom
+picom_backend='glx'
+picom_corner='10'
+picom_shadow_r='20'
+picom_shadow_o='0.60'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='1'
+bspwm_gap='15'
+bspwm_sratio='0.50'
